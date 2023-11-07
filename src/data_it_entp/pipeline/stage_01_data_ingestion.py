abbcef9bd45e991d250b21e557e6e0b0401d2ab4
Krunal Shambharkar <krunalss@outlook.com>
2023-11-07 18:25:33 +0530

    data ingestion pipeline completed

---
+++ src/data_it_entp/pipeline/stage_01_data_ingestion.py
@@ -0,0 +1,29 @@
+from data_it_entp.config.configuration import ConfigurationManager
+from data_it_entp.components.data_ingestion import DataIngestion
+from data_it_entp import logger
+
+STAGE_NAME = "Data Ingestion stage"
+
+class DataIngestionTrainingPipeline:
+    def __init__(self):
+        pass
+    #ye class kuch nhi karra hai yaha pe
+
+    def main(self):
+        config = ConfigurationManager()
+        data_ingestion_config = config.get_data_ingestion_config()
+        data_ingestion = DataIngestion(config=data_ingestion_config)
+        data_ingestion.download_file()
+        data_ingestion.extract_zip_file()
+
+
+    
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
