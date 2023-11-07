fa7bed00733312cda475b755745060fa686df2c3
Krunal Shambharkar <krunalss@outlook.com>
2023-11-07 19:07:58 +0530

    data validation  pipeline completed

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 from data_it_entp import logger
 from data_it_entp.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from data_it_entp.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+
 
 STAGE_NAME = "Data Ingestion stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataIngestionTrainingPipeline()
    data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_validation = DataValidationTrainingPipeline()
+   data_validation.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
