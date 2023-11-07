aa3ae57cb17a935a623cc9a26b1b320dd4d921fe
Krunal Shambharkar <krunalss@outlook.com>
2023-11-07 19:35:59 +0530

    transformation pipeline completed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from data_it_entp import logger
 from data_it_entp.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from data_it_entp.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from data_it_entp.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -18,6 +19,16 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_validation = DataValidationTrainingPipeline()
    data_validation.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Data Transformation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataTransformationTrainingPipeline()
+   data_ingestion.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
